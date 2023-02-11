ebc64f583b8b5c98f709b2af834a4ef6cfbf5dd0
Ashwin Jose <115637412+ajashwin@users.noreply.github.com>
2023-02-11 12:24:29 +0530

    Update ShowPosts.js

--- src/ShowPosts.js
+++ src/ShowPosts.js
@@ -6,7 +6,7 @@ const ShowPosts = () => {
 
     const fetchAllPosts = async () => {  //fetchallposts is a function that'll be going to make an api call for all
         
-      const resp = await fetch('http://localhost:8080/all')
+      const resp = await fetch('https://gid-backend-10x.onrender.com/all')
       setServerResponse(await resp.json())
     }
 
@@ -25,7 +25,7 @@ const ShowPosts = () => {
             //rendering images in browser and mapping it(iterating it) since its a result array
             // return <img src={post?.image_file} key={index}/>
             //but images cannot be rendered like this, we have to add api as well, lets give api of backend which will give image name as well as image address
-            return <img width={400} height={500} style={{border:'2px solid black'}} src= {`http://localhost:8080/images/${post.image_file}`} key={index}/>
+            return <img width={400} height={500} style={{border:'2px solid black'}} src= {`https://gid-backend-10x.onrender.com/images/${post.image_file}`} key={index}/>
             //then go to index.js, we'll use get api for there for images
           })
         } 
