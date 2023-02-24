fdb771d0196e598a20ec66644da2656b5c1beefa
Ashwin Jose <115637412+ajashwin@users.noreply.github.com>
2023-02-24 17:35:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import AddNewPost from './AddNewPost';
-import {BrowserRouter, Route,  Routes} from "react-router-dom"
+import {BrowserRouter, Link, Route,  Routes} from "react-router-dom"
 import ShowPosts from './ShowPosts';
 
-const App = () => {
+const Homepage = () => {
+  return <div>
+    <Link to="post" title='Post a new post'/>
+    <Link to="view" title='See all posts'/>
+  </div>
+}
+
+
+export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Homepage/>}/>
         <Route path="/post" element = {<AddNewPost/>}/>
         <Route path="/view" element={<ShowPosts/>}/>
       </Routes>
@@ -14,5 +23,4 @@ const App = () => {
   )
 }
 
-export default App
 
